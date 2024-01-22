868a3fde854d649ff2cfc3005f8a86bffd5b540d
Reoden <staraino_o@163.com>
2024-01-22 20:10:39 +0800

    08_Conditions

---
+++ 08_Conditions/global.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+var Line int
+var Column int
+var Putback rune
+var InputFilePtr *os.File
+var OutputFilePtr *os.File
+var OutputFile *bufio.Writer
+var InputFile *bufio.Reader
+var TokenStr = []string{"+", "-", "*", "/", "intlit"}
+var T Token
+var OpPrec = []int{
+	0, 10, 10, // T_EOF, T_PLUS, T_MINUS
+	20, 20, // T_STAR, T_SLASH
+	30, 30, // T_EQ, T_NEQ
+	40, 40, 40, 40, //T_LT, T_GT, T_LE, T_GE
+}
+var LastScannedIdent string
+
+const (
+	T_EOF = iota
+	// calc
+	T_PLUS
+	T_MINUS
+	T_STAR
+	T_SLASH
+	T_EQ
+	T_NEQ
+	T_LT
+	T_GT
+	T_LE
+	T_GE
+
+	//no precedence
+	T_INTLIT
+	T_NEWLINE
+	T_PRINT
+	T_ASSIGN
+	T_LBRACE
+	T_RBRACE
+	T_LPAREN
+	T_RPAREN
+
+	// keyword
+	T_VAR
+	T_IDENT
+	T_IF
+	T_ELSE
+)
+
+const (
+	A_EOF_PLACEHOLDED = iota
+	A_ADD
+	A_SUBTRACT
+	A_MULTIPLY
+	A_DIVIDE
+	A_EQ
+	A_NEQ
+	A_LT
+	A_GT
+	A_LE
+	A_GE
+
+	A_INTLIT
+	A_ASSIGNVAL
+	A_ASSIGN
+	A_IDENT
+	A_PRINT
+	A_GLUETO
+	A_IF
+)
---
+++ 08_Conditions/statements.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func compundStatements() *AstNode {
+	var left, tree *AstNode
+	left = nil
+	for isCurrentTokenNewLine() {
+		matchNewLine()
+	}
+
+	matchLBrace()
+	for 1 == 1 {
+		for isCurrentTokenNewLine() {
+			matchNewLine()
+		}
+
+		switch T.token {
+		case T_PRINT:
+			tree = printStatement()
+			break
+		case T_VAR:
+			varDeclaration()
+			tree = nil
+			break
+		case T_IDENT:
+			tree = assignmentStatement()
+			break
+		case T_IF:
+			tree = ifStatement()
+			break
+		case T_RBRACE:
+			matchRBrace()
+			return left
+		default:
+			log.Fatalf("Syntax error, token line %d column %d\n", Line, Column)
+			os.Exit(6)
+		}
+
+		OutputFile.Flush()
+		if tree != nil {
+			if left == nil {
+				left = tree
+			} else {
+				left = makeAstNode(A_GLUETO, left, nil, tree, -1, -1)
+			}
+		}
+	}
+
+	return nil
+}
+
+func assignmentStatement() *AstNode {
+	var left, right, tree *AstNode
+	matchIdent()
+	id, err := findGlobalSymbol(LastScannedIdent)
+	if err != nil {
+		log.Fatalf("Undeclared variable %s", LastScannedIdent)
+		os.Exit(7)
+	}
+
+	left = makeLeaf(A_ASSIGNVAL, -1, id)
+	matchToken(T_ASSIGN, "=")
+	right = binExpr(0)
+	tree = makeAstNode(A_ASSIGN, left, nil, right, 0, -1)
+	fmt.Fprint(OutputFile, interpretAST(tree))
+	OutputFile.Flush()
+
+	return tree
+}
+
+func printStatement() *AstNode {
+	var n *AstNode
+	matchToken(T_PRINT, "print")
+	n = binExpr(0)
+	return mkastunary(A_PRINT, n, -1, -1)
+}
+
+func ifStatement() *AstNode {
+	var condAST, trueAST, falseAST *AstNode
+	matchToken(T_IF, "if")
+	matchLPAREN()
+
+	condAST = binExpr(0)
+
+	if condAST.op < A_EQ || condAST.op > A_GE {
+		log.Fatalf("Comparison is not returning a boolean on line %d\n", Line)
+	}
+
+	matchRPAREN()
+	trueAST = compundStatements()
+
+	if T.token == T_ELSE {
+		scan(&T)
+		falseAST = compundStatements()
+	}
+
+	return makeAstNode(A_IF, condAST, trueAST, falseAST, -1, -1)
+}
